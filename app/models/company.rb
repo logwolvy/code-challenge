8e9a9c745f881a7ed3f21a561b10e9c2450cfb38
logwolvy <18429813+logwolvy@users.noreply.github.com>
2020-07-08 12:14:39 +0530

    Extract email validation logic

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,24 +1,27 @@
 # frozen_string_literal: true
 
 class Company < ApplicationRecord
+  EMAIL_DOMAIN_PATTERN = /\b.+@getmainstreet\.com\z/.freeze
   has_rich_text :description
 
   # Validations
-  # TODO: combine both in custom validator
-  validates :email, format: {
-    with: URI::MailTo::EMAIL_REGEXP,
-    message: 'must be valid'
-  }, allow_blank: true
-
-  validates :email, format: {
-    with: /\b.+@getmainstreet\.com\z/,
-    message: 'must have a getmainstreet.com domain'
-  }, allow_blank: true
+  validate :email_format, if: proc { |c| c.email.present? }
 
   # Callbacks
-  before_save :populate_city_and_state, if: proc { |company| company.zip_code.present? }
+  before_save :populate_city_and_state, if: proc { |c| c.zip_code.present? }
 
   private
+
+  def email_format
+    return if URI::MailTo::EMAIL_REGEXP.match?(email) &&
+              EMAIL_DOMAIN_PATTERN.match?(email)
+
+    if URI::MailTo::EMAIL_REGEXP.match?(email)
+      errors.add(:email, 'must have a getmainstreet.com domain')
+    else
+      errors.add(:email, 'must be valid')
+    end
+  end
 
   def populate_city_and_state
     decoded_zipcode = ZipCodes.identify(zip_code)
